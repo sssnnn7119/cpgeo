98a270a8c10546ffeede1e8149cbc3aeb8d42961
24391 <243919588@qq.com>
2025-07-15 10:55:54 +0800

    20250715 Threshold and k_neighbors

--- CPGEO/base.py
+++ CPGEO/base.py
@@ -82,6 +82,9 @@ class _CPGEO_base:
         self.threshold: torch.Tensor = None
         """Influence radius for each knot point [num_knots]"""
 
+        self.k_neighbors: int = 10
+        """Number of nearest neighbors to consider for threshold calculation"""
+
         self._weight_tree = None
         """Data structure for efficient weight computation"""
 
@@ -507,7 +510,7 @@ class _CPGEO_base:
         
         # Number of knots and k-value for k-th nearest neighbor
         num_knots = self.knots.shape[1]
-        k_neighbors = 25  # Use 10th nearest neighbor as default
+        k_neighbors = self.k_neighbors  # Use 10th nearest neighbor as default
         
         # Convert knots to contiguous numpy arrays for C++ interop
         knots_np = self.knots.T.detach().cpu().numpy().astype(np.float64).copy(order='C')
--- build/lib/CPGEO/utils/C_api.py
+++
@@ -1,7 +0,0 @@
-
-import ctypes
-import os
-current_path = os.path.dirname(os.path.abspath(__file__))
-
-faceApi = ctypes.CDLL(os.path.dirname(current_path) + '/dlls/CPGEO.dll')
-faceApi.build_trees.restype = ctypes.POINTER(ctypes.c_void_p)
